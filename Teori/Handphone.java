ee41c84d3c0e9b173360cae11d3d9ead07e5bf11
EkaLidya <lidyae6@gmail.com>
2024-09-26 13:56:27 +0700

    Add files via upload

---
+++ Teori/Handphone.java
@@ -0,0 +1,23 @@
+package Teori;
+
+public class Handphone {
+    protected String merk;
+    protected String model;
+
+    public Handphone(String merk, String model) {
+        this.merk = merk;
+        this.model = model;
+    }
+
+    public void nyalakan() {
+        System.out.println("Handphone dinyalakan.");
+    }
+
+    public void matikan() {
+        System.out.println("Handphone dimatikan.");
+    }
+
+    public void telepon(String nomor) {
+        System.out.println("Memanggil nomor " + nomor);
+    }
+}
